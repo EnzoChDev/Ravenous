ef04b96c26e93f6ad54cb8cca4dbefb581d93344
EnzoChDev <enzo.chavanne.dev@gmail.com>
2019-04-09 22:52:09 +0200

    Updates

---
+++ precache-manifest.c350d0976cbee10a25f5943e4cdbd2f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5ddfa0b767d8254e6289",
+    "url": "/Ravenous/static/js/runtime~main.9078551a.js"
+  },
+  {
+    "revision": "b330ea94087e8e3f7c47",
+    "url": "/Ravenous/static/js/main.46153ff2.chunk.js"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/Ravenous/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "b330ea94087e8e3f7c47",
+    "url": "/Ravenous/static/css/main.402dcda0.chunk.css"
+  },
+  {
+    "revision": "978cb5eb8c92ae4c3159cb000ce9498e",
+    "url": "/Ravenous/index.html"
+  }
+];
